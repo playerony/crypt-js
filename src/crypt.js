239b0d6035120a9c4338562d3cc6c82447d7ce1e
playerony <pawel.wojtasinski.1995@gmail.com>
2018-08-05 20:09:50 +0200

    Add selection of algorithms

--- src/crypt.js
+++ src/crypt.js
@@ -1,8 +1,32 @@
 const MD5 = require('./algorithms/MD5').MD5;
 const SHA256 = require('./algorithms/SHA256').SHA256;
 const ADFGVX = require('./algorithms/ADFGVX').ADFGVX;
-const text = "WORK";
 
-console.log(MD5(text));
-console.log(SHA256(text));
-console.log(ADFGVX(text));
+var generateResponse = (algorithm, result) => {
+    return {
+        algorithm,
+        result
+    }
+}
+
+var encode = (options) => {
+    var algorithm = options.algorithm;
+    var text = options.value;
+
+    switch(algorithm) {
+        case 'MD5':
+            return generateResponse(algorithm, MD5(text));
+
+        case 'SHA256':
+            return generateResponse(algorithm, SHA256(text));
+
+        default:
+        case 'ADFGVX':
+            return generateResponse('ADFGVX', ADFGVX(text));
+    }
+}
+
+console.log(encode({
+    algorithm: 'SHA256',
+    value: 'WORD'
+}))
