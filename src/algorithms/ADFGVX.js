e20ac53d301ff09534212c6dde209e8ee31e55b6
playerony <pawel.wojtasinski.1995@gmail.com>
2018-08-05 19:33:44 +0200

    Create ADFGVX algorithm

---
+++ src/algorithms/ADFGVX.js
@@ -0,0 +1,58 @@
+exports.ADFGVX = function(string) {
+    var word = ['*', 'A', ',', '^', '!', 'a', 'K', ')', '/', ';', 'G', 'f', '@', 'D', '~', 'm', '(', 'N', '+', '-', '1', '2', '3', '4', '5'];
+    
+    var generateTableOfLetters = () => {
+        var result = [];
+        var lineOfLetters = [];
+        var counter = 0;
+
+        for(var i=0; i<512; i++) {
+            lineOfLetters.push(String.fromCharCode(i));
+            counter++;
+
+            if(counter % 23 === 0) {
+                counter = 0;
+                result.push(lineOfLetters);
+                lineOfLetters = [];
+            }
+        }
+
+        if(lineOfLetters.length > 0) {
+            result.push(lineOfLetters);
+            lineOfLetters = [];
+        }
+
+        return result;
+    }
+
+    var lettersTable = generateTableOfLetters();
+
+    var getLetter = (x, y) => {
+        return lettersTable[y][x];
+    }
+
+    var findLetter = (char) => {
+        for(var i=0; i<lettersTable.length; i++) {
+            var line = lettersTable[i];
+
+            for(var j=0; j<line.length; j++) {
+                var letter = getLetter(j, i);
+
+                if(letter === char)
+                    return word[j] + word[i];
+            }
+        }
+
+        return '';
+    }
+
+    var encodeWord = (string) => {
+        var result = '';
+        for(var i=0; i<string.length; i++)
+            result += findLetter(string[i]);
+
+        return result;
+    }
+
+    return encodeWord(string);
+}
--- src/crypt.js
+++ src/crypt.js
@@ -1,6 +1,8 @@
 const MD5 = require('./algorithms/MD5').MD5;
 const SHA256 = require('./algorithms/SHA256').SHA256;
+const ADFGVX = require('./algorithms/ADFGVX').ADFGVX;
 const text = "WORK";
 
 console.log(MD5(text));
 console.log(SHA256(text));
+console.log(ADFGVX(text));
