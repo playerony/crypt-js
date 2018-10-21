5593e42b7243d1a9ae0c45e0761874084bc68633
playerony <pawel.wojtasinski.1995@gmail.com>
2018-10-21 16:40:38 +0200

    Update README.md file

--- example.js
+++
@@ -1,9 +0,0 @@
-const hash = require("./index").hash;
-const algorithms = require("./index").algorithms;
-
-console.log(
-  hash({
-    algorithms: [algorithms.DES, algorithms.MD5, algorithms.ADFGVX],
-    value: "WOrK"
-  })
-);
